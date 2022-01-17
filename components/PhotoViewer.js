0db2b2813bcaabc627cc8b473d207322fba543c3
Oubi <lito5443@gmail.com>
2022-01-17 20:53:57 +0200

    fucking interpolate

--- components/PhotoViewer.js
+++ components/PhotoViewer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Dimensions, FlatList, Image, Text, View } from 'react-native';
-import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import { Dimensions, FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import Animated, { Extrapolation, interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 const IMAGES = [
     require('../images/planet1.png'),
@@ -35,20 +35,31 @@ const ITEM_IDENT = 20
 
 const PhotoViewer = () => {
 
-    const scrollX = useSharedValue(0)
+    const scrollY = useSharedValue(0)
 
     const onScroll = useAnimatedScrollHandler({
         onScroll: (e) => {
-            console.log(e.contentOffset.y)
+            scrollY.value = e.contentOffset.y
         },
     });
 
-    const renderItem = ({item}) => {
+    const Planet = ({item, index}) => {
+        const scaleAnim = useAnimatedStyle(() => {
+            const scale = interpolate(
+                scrollY.value, 
+                [ITEM_HEIGHT, ITEM_HEIGHT*index, ITEM_HEIGHT*(index+3)],
+                [1, 1, 0],
+                { extrapolateRight: Extrapolation.CLAMP });
+            return {
+              transform: [{ scale: scale }],
+            };
+        });
+
         return (
-            <View style={{backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center', width: width, height: ITEM_HEIGHT, borderWidth: 1, borderColor: 'white'}}>
-                <View style={{margin: 50, backgroundColor: 'darkgreen', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-                    <Image resizeMode={'contain'} style={{backgroundColor: 'black', width: width-ITEM_IDENT, height: ITEM_HEIGHT-ITEM_IDENT}} source={item}/>
-                </View>
+            <View style={style.itemContainer}>
+                <Animated.View style={[style.item, scaleAnim]}>
+                    <Image resizeMode={'contain'} style={{width: '100%', height: '100%'}} source={item}/>
+                </Animated.View>
             </View>
         )
     }
@@ -58,8 +69,10 @@ const PhotoViewer = () => {
             <Animated.FlatList
                 bounces={false}
                 data={IMAGES}
-                renderItem={renderItem}
-                snapToInterval={ITEM_HEIGHT}
+                renderItem={({ item, index }) => {
+                    return <Planet item={item} index={index}/>
+                    }}
+                //snapToInterval={ITEM_HEIGHT}
                 style={{flex: 1}}
                 onScroll={onScroll}
             />
@@ -69,3 +82,21 @@ const PhotoViewer = () => {
 }
 
 export default PhotoViewer
+
+const style = StyleSheet.create({
+    item: {
+        backgroundColor: 'black', 
+        width: width-ITEM_IDENT, 
+        height: ITEM_HEIGHT-ITEM_IDENT, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        borderRadius: 12, 
+        padding: 20
+    },
+    itemContainer: {
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: width, height: 
+        ITEM_HEIGHT
+    }
+})
