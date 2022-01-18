9b6713369894da5ae8a9f026ee56f421f5c0e8f2
Oubi256 <oubi256@iMac.local>
2022-01-18 17:07:05 +0200

    Worked!

--- components/LoadScreen.js
+++ components/LoadScreen.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import Animated, { Easing, multiply, sin, useAnimatedStyle, useSharedValue, withSequence, withTiming } from "react-native-reanimated";
+import React, { useState } from "react";
+import { Button, Dimensions, Image, ImageBackground, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Animated, { cond, Easing, multiply, sin, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withTiming } from "react-native-reanimated";
 
 const { width, height } = Dimensions.get('window');
 
@@ -15,32 +15,39 @@ const LoadScreen = () => {
         
         rocketX.value = 0
         rocketY.value = 0
-        rocketX.value = withSequence(
-            withTiming(rocketPathWidth, {
-                duration: 3000,
-                easing: Easing.bezier(1, 1, 1, 1),
-            }),
-            withTiming(0, {
-                duration: 3000,
-                easing: Easing.bezier(1, 0.8, 0.8, 0.2),
-            }),
+        rocketX.value = withRepeat(
+            withDelay(1000,
+                withSequence(
+                    withTiming(rocketPathWidth, {
+                        duration: 3000,
+                        easing: Easing.bezier(1, 1, 1, 1),
+                    }),
+                    withTiming(0, {
+                        duration: 3000,
+                        easing: Easing.bezier(1, 0.8, 0.8, 0.2),
+                    }),
+                ),
+            ), -1
         )
 
-        rocketY.value = withSequence(
-            withTiming(50, {
-                duration: 3000,
-                easing: Easing.bezier(1, 1, 1, 1)
-            }, () => isGoBack.value = true),
-            withTiming(10, {
-                duration: 3000,
-                easing: Easing.bezier(1, 0.8, 0.8, 0.2),
-            }, () => isGoBack.value = false),
+        rocketY.value = withRepeat(
+            withDelay(1000,
+                withSequence(
+                    withTiming(50, {
+                        duration: 3000,
+                        easing: Easing.bezier(1, 1, 1, 1)
+                    }, () => isGoBack.value = true),
+                    withTiming(10, {
+                        duration: 3000,
+                        easing: Easing.bezier(1, 0.8, 0.8, 0.2),
+                    }, () => isGoBack.value = false),
+                ),
+            ), -1
         )
     }
     
     
     const rocketPath = useAnimatedStyle(() => {
-
         const transY = parseInt(`${26*Math.sin(rocketY.value/6)}`)
         const rotateZ = isGoBack.value ? `${270+(26*Math.sin((rocketY.value+8)/6))}deg` : `${90+(26*Math.sin((rocketY.value+8)/6))}deg`
         return {
@@ -52,22 +59,109 @@ const LoadScreen = () => {
         };
     });
 
+    const handShake = 
 
+    flyRocket()
     return (
         <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{flex: 1}}>TEXT-TEXT-TEXT</Text>
-                <View style={[{flex: 6, backgroundColor: 'pink', width: '100%'}]}>
-                    <Animated.Image 
-                        source={require('../images/rocket.png')} 
-                        resizeMode={'contain'} 
-                        style={[style.rocket, rocketPath]}
-                    />
-                </View>
-            <View style={{flex: 8}}>
-            <Button title="SpaceX" onPress={() => flyRocket()}/>
+            <View style={{flex: 1, justifyContent:'center'}}>
+                <Text style={style.loadText}>Rise and Grind with Da...</Text>
+            </View>
+            
+            <View style={[{flex: 6, width: '100%'}]}>
+                <Animated.Image 
+                    source={require('../images/rocket.png')} 
+                    resizeMode={'contain'} 
+                    style={[style.rocket, rocketPath]}
+                />
+            </View>
 
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <Text style={style.loadText}>Loading</Text>
+                <LoadingDot/>
+            </View>
+            
+            <View style={{flex: 8, margin: 10, justifyContent: 'center', alignItems: 'center'}}>
+                <HandShakeMan/>
             </View>
         </SafeAreaView>
+    )
+}
+
+
+
+const LoadingDot = (animateStyle) => {
+
+    const dotState = useSharedValue(0); 
+    dotState.value = withRepeat(
+        withDelay(500,
+                withTiming(4, {
+                    duration: 3000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                }),
+        ), -1
+    )
+
+    const dotOpacity = (index) => useAnimatedStyle(() => {
+        return {
+            opacity: dotState.value>index ? 
+                withTiming(1, {
+                    duration: 1000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                }) : 
+                withTiming(0, {
+                    duration: 1000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                })
+        }
+    });
+
+    return (
+        <>
+        <Animated.Text style={[style.loadText, dotOpacity(1)]}>.</Animated.Text>
+        <Animated.Text style={[style.loadText, dotOpacity(2)]}>.</Animated.Text>
+        <Animated.Text style={[style.loadText, dotOpacity(3)]}>.</Animated.Text>
+        </>
+    )
+}
+
+
+
+const HandShakeMan = () => {
+    const handShake = useSharedValue(0); 
+    handShake.value = withRepeat(
+        withDelay(500,
+                withTiming(4, {
+                    duration: 3000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                }),
+        ), -1
+    )
+
+    const handShakeRotate = (index) => useAnimatedStyle(() => {
+        return {
+            opacity: handShake.value>index ? 
+                withTiming(1, {
+                    duration: 1000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                }) : 
+                withTiming(0, {
+                    duration: 1000,
+                    easing: Easing.bezier(1, 1, 1, 1),
+                })
+        }
+    });
+
+    return (
+        <ImageBackground source={require('../images/Body.png')} resizeMode={'center'} style={{width: 200, height: 200}}>
+            <Animated.View style={[{width: 100, height: 100, justifyContent: 'center' ,alignItems: 'center'}, {transform: [{translateX: 30}, {translateY: 40}, handShakeRotate]}]}>
+                <Image 
+                    source={require('../images/Hand.png')} 
+                    resizeMode={'center'}
+                    style={{flex: 1, transform: [{translateY: -10}]}}
+                />
+            </Animated.View>
+        </ImageBackground>
     )
 }
 
@@ -80,5 +174,9 @@ const style = StyleSheet.create({
         position: "absolute",
         bottom: '50%',
         left: -50,
+    },
+    loadText: {
+        fontWeight: 'bold',
+        color: '#164687',
     }
 })
